202a7eca10eaa09a8b5280b7a71a2b7b0450711b
Ilya Makarov <feesler@gmail.com>
2021-04-12 00:27:04 +0300

    * Fix e-mail link at footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -42,7 +42,7 @@ function Footer() {
             <h5>Контакты:</h5>
             <a className="footer-contacts-phone" href="tel:+7-495-790-35-03">+7 495 79 03 5 03</a>
             <span className="footer-contacts-working-hours">Ежедневно: с 09-00 до 21-00</span>
-            <a className="footer-contacts-email" href="mailto:{contacts.email}">{contacts.email}</a>
+            <a className="footer-contacts-email" href={`mailto:${contacts.email}`}>{contacts.email}</a>
             <div className="footer-social-links">
               <div className="footer-social-link footer-social-link-twitter"></div>
               <div className="footer-social-link footer-social-link-vk"></div>
