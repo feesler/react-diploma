29430e8c25f4c7e9d48c36cec70a0f3e5b72d111
Ilya Makarov <feesler@gmail.com>
2021-04-13 00:05:23 +0300

    * Move order form to separate component

---
+++ frontend/src/components/OrderForm.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { changeOrderField, orderRequest } from '../store/cartSlice';
+
+const orderCardStyle = {
+  maxWidth: '30rem',
+  margin: '0 auto',
+};
+
+function OrderForm() {
+  const { items, owner } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const field = { name: e.target.id };
+
+    if (e.target.type === 'checkbox') {
+      field.value = e.target.checked;
+    } else {
+      field.value = e.target.value;
+    }
+
+    dispatch(changeOrderField(field));
+  };
+
+  const handleSubmit = () => {
+    const data = {
+      owner: { phone: owner.phone, address: owner.address },
+      items: items.map((item) => ({ id: item.id, price: item.price, count: item.quantity })),
+    };
+
+    dispatch(orderRequest(data));
+  };
+
+  return (
+    <div className="card" style={orderCardStyle}>
+      <form className="card-body">
+        <div className="form-group">
+          <label htmlFor="phone">Телефон</label>
+          <input
+            className="form-control"
+            id="phone"
+            placeholder="Ваш телефон"
+            value={owner.phone}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Адрес доставки</label>
+          <input
+            className="form-control"
+            id="address"
+            placeholder="Адрес доставки"
+            value={owner.address}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="form-group form-check">
+          <input type="checkbox" className="form-check-input" id="agreement" onChange={handleChange} value={owner.agreement} />
+          <label className="form-check-label" htmlFor="agreement">Согласен с правилами доставки</label>
+        </div>
+        <button
+          type="submit"
+          className="btn btn-outline-secondary"
+          disabled={!owner.agreement}
+          onClick={handleSubmit}
+        >Оформить</button>
+      </form>
+    </div>
+  );
+}
+
+export default OrderForm;
--- frontend/src/views/CartView.jsx
+++ frontend/src/views/CartView.jsx
@@ -1,43 +1,15 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import View from './View.jsx';
-import { removeByIndex, changeOrderField, orderRequest } from '../store/cartSlice';
+import { removeByIndex } from '../store/cartSlice';
+import OrderForm from '../components/OrderForm.jsx';
 
 function CartView() {
-  const {
-    items,
-    owner,
-  } = useSelector((state) => state.cart);
+  const { items } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
-
-  const orderCardStyle = {
-    maxWidth: '30rem',
-    margin: '0 auto',
-  };
-
-  const handleChange = (e) => {
-    const field = { name: e.target.id };
-
-    if (e.target.type === 'checkbox') {
-      field.value = e.target.checked;
-    } else {
-      field.value = e.target.value;
-    }
-
-    dispatch(changeOrderField(field));
-  };
 
   const handleDelete = (index) => {
     dispatch(removeByIndex(index));
-  };
-
-  const handleSubmit = () => {
-    const data = {
-      owner: { phone: owner.phone, address: owner.address },
-      items: items.map((item) => ({ id: item.id, price: item.price, count: item.quantity })),
-    };
-
-    dispatch(orderRequest(data));
   };
 
   const totalPrice = items.reduce((prev, item) => prev + (item.price * item.quantity), 0);
@@ -80,41 +52,7 @@ function CartView() {
       </section>
       <section className="order">
         <h2 className="text-center">Оформить заказ</h2>
-        <div className="card" style={orderCardStyle}>
-          <form className="card-body">
-            <div className="form-group">
-              <label htmlFor="phone">Телефон</label>
-              <input
-                className="form-control"
-                id="phone"
-                placeholder="Ваш телефон"
-                value={owner.phone}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="address">Адрес доставки</label>
-              <input
-                className="form-control"
-                id="address"
-                placeholder="Адрес доставки"
-                value={owner.address}
-                onChange={handleChange}
-              />
-            </div>
-            <div className="form-group form-check">
-              <input type="checkbox" className="form-check-input" id="agreement" onChange={handleChange} value={owner.agreement} />
-              <label className="form-check-label" htmlFor="agreement">Согласен с правилами доставки</label>
-            </div>
-            <button
-              type="submit"
-              className="btn btn-outline-secondary"
-              disabled={!owner.agreement}
-              onClick={handleSubmit}
-            >Оформить</button>
-          </form>
-
-        </div>
+        <OrderForm />
       </section>
     </View >
   );
