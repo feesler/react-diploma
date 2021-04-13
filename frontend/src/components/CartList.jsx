e38b9ecbff762559e0e95b986951c0f7b59b197a
Ilya Makarov <feesler@gmail.com>
2021-04-13 14:36:44 +0300

    + Add CartList component

---
+++ frontend/src/components/CartList.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { removeByIndex } from '../store/cartSlice';
+
+function CartList() {
+  const { items } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const handleDelete = (index) => {
+    dispatch(removeByIndex(index));
+  };
+
+  const totalPrice = items.reduce((prev, item) => prev + (item.price * item.quantity), 0);
+
+  return (
+    <section className="cart">
+      <h2 className="text-center">Корзина</h2>
+      { (items.length > 0)
+        ? (
+          <table className="table table-bordered">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Название</th>
+                <th scope="col">Размер</th>
+                <th scope="col">Кол-во</th>
+                <th scope="col">Стоимость</th>
+                <th scope="col">Итого</th>
+                <th scope="col">Действия</th>
+              </tr>
+            </thead>
+            <tbody>
+              {items.map((item, index) => (
+                <tr key={`cart_${index}`}>
+                  <th scope="row">{index + 1}</th>
+                  <td><Link to={`products/${item.id}.html`}>{item.title}</Link></td>
+                  <td>{item.size}</td>
+                  <td>{item.quantity}</td>
+                  <td>{item.price} руб.</td>
+                  <td>{item.price * item.quantity} руб.</td>
+                  <td><button className="btn btn-outline-danger btn-sm" onClick={() => handleDelete(index)}>Удалить</button></td>
+                </tr>
+              ))}
+
+              <tr>
+                <td colSpan="5" className="text-right">Общая стоимость</td>
+                <td>{totalPrice} руб.</td>
+              </tr>
+            </tbody>
+          </table>
+        )
+        : (
+          <h6 className="text-center">Ваша корзина пуста</h6>
+        )
+      }
+    </section>
+  );
+}
+
+export default CartList;
--- frontend/src/views/CartView.jsx
+++ frontend/src/views/CartView.jsx
@@ -1,22 +1,14 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
 import { Link, useLocation } from 'react-router-dom';
 import View from './View.jsx';
-import { removeByIndex } from '../store/cartSlice';
+
 import OrderForm from '../components/OrderForm.jsx';
+import CartList from '../components/CartList.jsx';
 
 function CartView() {
   const { search } = useLocation();
   const params = new URLSearchParams(search);
   const submitResult = params.get('submit');
-  const { items } = useSelector((state) => state.cart);
-  const dispatch = useDispatch();
-
-  const handleDelete = (index) => {
-    dispatch(removeByIndex(index));
-  };
-
-  const totalPrice = items.reduce((prev, item) => prev + (item.price * item.quantity), 0);
 
   if (submitResult === 'ok') {
     return (
@@ -32,41 +24,7 @@ function CartView() {
 
   return (
     <View>
-      <section className="cart">
-        <h2 className="text-center">Корзина</h2>
-        <table className="table table-bordered">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Название</th>
-              <th scope="col">Размер</th>
-              <th scope="col">Кол-во</th>
-              <th scope="col">Стоимость</th>
-              <th scope="col">Итого</th>
-              <th scope="col">Действия</th>
-            </tr>
-          </thead>
-          <tbody>
-            {items.map((item, index) => (
-              <tr key={`cart_${index}`}>
-                <th scope="row">{index + 1}</th>
-                <td><Link to={`products/${item.id}.html`}>{item.title}</Link></td>
-                <td>{item.size}</td>
-                <td>{item.quantity}</td>
-                <td>{item.price} руб.</td>
-                <td>{item.price * item.quantity} руб.</td>
-                <td><button className="btn btn-outline-danger btn-sm" onClick={() => handleDelete(index)}>Удалить</button></td>
-              </tr>
-            ))}
-
-            <tr>
-              <td colSpan="5" className="text-right">Общая стоимость</td>
-              <td>{totalPrice} руб.</td>
-            </tr>
-          </tbody>
-        </table>
-      </section>
-
+      <CartList />
       <OrderForm />
     </View >
   );
