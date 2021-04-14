aa31c83380eeb633743b5f81a53c0ee37cd4523b
Ilya Makarov <feesler@gmail.com>
2021-04-14 21:33:47 +0300

    * Fix setting error at categories slice

--- frontend/src/store/categoriesSlice.js
+++ frontend/src/store/categoriesSlice.js
@@ -23,7 +23,7 @@ const categoriesSlice = createSlice({
     categoriesReadFailure: (state, action) => ({
       ...state,
       loading: false,
-      error: action.payload.error,
+      error: action.payload,
     }),
   },
 });
